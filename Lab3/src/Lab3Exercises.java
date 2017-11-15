bad77100be39ad839a2ccbe8f66610ef216c8e93
4010a1 <4010a1@guineo.ece.uprm.edu>
2017-11-15 12:12:22 -0400

    The changes made to the program were the following: the position of the frame was set close to the center, the size of the frame was set to 200x200, and the setDefaultOperation was uncommented.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Alejandro Cocco");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
